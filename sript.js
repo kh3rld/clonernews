a5125c6f7624d2b2f70dd366b4741daba67de4fd
doonyango <datieno001@gmail.com>
2024-10-20 06:37:08 +0300

    feat: add a function to fetch stories from api

--- sript.js
+++ sript.js
@@ -2,7 +2,12 @@
 
 const API_BASE_URL = 'https://hacker-news.firebaseio.com/v0';
 
-async function fetchItem(id) {
+async function fetchItems(id) {
     const response = await fetch(`${API_BASE_URL}/item/${id}.json`);
+    return response.json();
+}
+
+async function fetchStories(type) {
+    const response = await fetch(`${API_BASE_URL}/${type}stories.json`);
     return response.json();
 }
