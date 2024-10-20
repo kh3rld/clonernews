f465ae2f45b48d9e5977867df3bcaf1b49ef8c69
doonyango <datieno001@gmail.com>
2024-10-20 06:35:24 +0300

    feat: add function to fetch items from api

--- sript.js
+++ sript.js
@@ -1,1 +1,8 @@
 //clonernews scripts
+
+const API_BASE_URL = 'https://hacker-news.firebaseio.com/v0';
+
+async function fetchItem(id) {
+    const response = await fetch(`${API_BASE_URL}/item/${id}.json`);
+    return response.json();
+}
